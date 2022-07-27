2bcfcf0372c90079e98b3a55f835fa9b22efd1bb
paulmcpg <84036679+paulmcpg@users.noreply.github.com>
2022-07-27 16:35:19 -0400

    Add project files.

---
+++ TestProject/Infrastructure/DbTestbase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using xAI.Data;
+using System;
+using TestProject.Infrastructure;
+
+namespace xAI.Tests.Infrastructure
+{
+    public class xAITestBase : IDisposable
+    {
+        protected readonly xAIContext _context;
+
+        public xAITestBase()
+        {
+            var options = new DbContextOptionsBuilder<xAIContext>()
+                .EnableSensitiveDataLogging()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new xAIContext(options);
+
+            _context.Database.EnsureCreated();
+
+            xAIInitializer.Initialize(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+
+            _context.Dispose();
+        }
+    }
+}
---
+++ TestProject/Infrastructure/TestDbInit.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using xAI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xAI.Data;
+
+namespace TestProject.Infrastructure
+{
+    class xAIInitializer
+    {
+        public static void Initialize(xAIContext context)
+        {
+//[SEEDDBINIT]
+
+            Seed(context);
+
+        }
+
+        private static void Seed(xAIContext context)
+        {
+//[SEEDDB]
+        }
+    }
+}
+
